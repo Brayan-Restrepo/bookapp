853c49a62aa914095a1e875a8bd5d79aa3d2f090
Brayan Restrepo <brayan2293@gmail.com>
2024-05-24 16:12:30 -0500

    init

---
+++ ent/schema/book.go
@@ -0,0 +1,32 @@
+package schema
+
+import (
+	"time"
+
+	"entgo.io/ent"
+	"entgo.io/ent/schema/edge"
+	"entgo.io/ent/schema/field"
+)
+
+// Book holds the schema definition for the Book entity.
+type Book struct {
+	ent.Schema
+}
+
+// Fields of the Book.
+func (Book) Fields() []ent.Field {
+	return []ent.Field{
+		field.String("title"),
+		field.Time("created_at").Default(time.Now),
+	}
+}
+
+// Edges of the Book.
+func (Book) Edges() []ent.Edge {
+	return []ent.Edge{
+		edge.From("author", User.Type).
+			Ref("books").
+			Unique().
+			Required(),
+	}
+}
---
+++ server.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"bookapp/ent"
+	"bookapp/graph"
+	"context"
+	"github.com/99designs/gqlgen/graphql/handler"
+	"github.com/99designs/gqlgen/graphql/playground"
+	_ "github.com/lib/pq"
+	"log"
+	"net/http"
+	"os"
+)
+
+const defaultPort = "8080"
+
+func main() {
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = defaultPort
+	}
+
+	dsn := "host=localhost port=5432 user=root password=toor dbname=bookapp sslmode=disable"
+	client, err := ent.Open("postgres", dsn)
+
+	if err != nil {
+		log.Fatalf("failed opening connection to sqlite: %v", err)
+	}
+	defer client.Close()
+	// Run the auto migration tool.
+	if err := client.Schema.Create(context.Background()); err != nil {
+		log.Fatalf("failed creating schema resources: %v", err)
+	}
+
+	srv := handler.NewDefaultServer(graph.NewExecutableSchema(graph.Config{Resolvers: &graph.Resolver{
+		Client: client,
+	}}))
+
+	http.Handle("/", playground.Handler("GraphQL playground", "/query"))
+	http.Handle("/query", srv)
+
+	log.Printf("connect to http://localhost:%s/ for GraphQL playground", port)
+	log.Fatal(http.ListenAndServe(":"+port, nil))
+}
